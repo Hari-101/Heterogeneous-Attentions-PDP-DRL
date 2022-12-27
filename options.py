2b620d2ede23597766d18e791eddb257cb8ab214
Hari-101 <harinarayan101@gmail.com>
2022-12-27 18:46:33 +0530

    Added training regime options

--- options.py
+++ options.py
@@ -57,6 +57,9 @@ def get_options(args=None):
     parser.add_argument('--data_distribution', type=str, default=None,
                         help='Data distribution to use during training, defaults and options depend on problem.')
 
+    #Training Regime
+    parser.add_argument('--training_regime', default='RL', help='How to train the DNN?')
+
     # Misc
     parser.add_argument('--log_step', type=int, default=50, help='Log info every log_step steps')
     parser.add_argument('--log_dir', default='logs', help='Directory to write TensorBoard information to')
